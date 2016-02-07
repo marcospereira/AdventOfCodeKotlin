acd5fd823ef379c6dd23b73185f74d9306adaa36
Francois Campbell <campbell.francois@gmail.com>
2016-02-07 17:37:03 -0500

    Added DayRunner to run all at once

--- src/main/kotlin/com/github/marcospereira/Day.kt
+++ src/main/kotlin/com/github/marcospereira/Day.kt
@@ -7,13 +7,32 @@ import java.io.File
  */
 
 abstract class Day() {
+
     val file = File("src/main/resources/${javaClass.simpleName.toLowerCase()}.txt")
 
     abstract fun part1(): Any?
     abstract fun part2(): Any?
 
     fun run() {
-        println("Part 1: ${part1()}")
-        println("Part 2: ${part2()}")
+        println(javaClass.simpleName)
+        println("Part 1: ${runPart1()}")
+        println("Part 2: ${runPart2()}")
+        println("")
+    }
+
+    private fun runPart1(): Any? {
+        try {
+            return part1()
+        } catch (e: UnsupportedOperationException) {
+            return "Not implemented"
+        }
+    }
+
+    private fun runPart2(): Any? {
+        try {
+            return part2()
+        } catch (e: UnsupportedOperationException) {
+            return "Not implemented"
+        }
     }
 }
---
+++ src/main/kotlin/com/github/marcospereira/DayRunner.kt
@@ -0,0 +1,21 @@
+package com.github.marcospereira
+
+/**
+ * Created by francois on 2016-02-07.
+ */
+class DayRunner {
+    companion object {
+        val days = listOf(
+                Day1(),
+                Day2(),
+                Day3(),
+                //                Day4(),
+                Day5(),
+                Day6())
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            days.forEach { it.run() }
+        }
+    }
+}
