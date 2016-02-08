9e638a14fbd19c52774aa8aed933b01f0b8815fe
Francois Campbell <campbell.francois@gmail.com>
2016-02-08 16:06:03 -0500

    Day 8 part 2

--- src/main/kotlin/com/github/marcospereira/day8/Day8.kt
+++ src/main/kotlin/com/github/marcospereira/day8/Day8.kt
@@ -8,15 +8,14 @@ import com.github.marcospereira.Day
 class Day8 : Day() {
 
     override fun part1(): Any? = file.readLines().sumBy {
-        val memoryString = it.slice(1..it.length - 2)
+        it.length - it.slice(1..it.length - 2)
                 .replace("\\\\", " ")
                 .replace("\\\"", " ")
                 .replace(Regex("\\\\x[0-9a-fA-F]{2}"), " ")
-
-        return@sumBy it.length - memoryString.length
+                .length
     }
 
-    override fun part2(): Any? {
-        throw UnsupportedOperationException()
+    override fun part2() = file.readLines().sumBy {
+        it.replace("\\", "\\\\").length + it.replace("\"", "\\\"").length - 2 * it.length + 2
     }
 }
